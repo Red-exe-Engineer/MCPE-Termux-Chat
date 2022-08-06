edf9f0f478af9b22ef508175e473163bd3fde9dd
Red-exe-Engineer <90420989+Red-exe-Engineer@users.noreply.github.com>
2022-08-06 19:47:01 -0400

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,44 @@
+# Created by Wallee#8314/Red-exe-Engineer
+
+# Imports
+from mcpi.minecraft import Miencraft
+from os import system
+
+# Clear the screen
+system("clear")
+
+# Print a start message
+print("Type \"!exit\" to exit the program.\n - Wallee\n")
+
+# Repeat forever
+while True:
+
+    # Get a string from the user
+    message = input("Chat: ")
+
+    # Check if the string is "!exit"
+    if message == "!exit":
+
+        # Break out of the loop
+        break
+
+    # May not be able to connect to Minecraft
+    try:
+
+        # Connect to Minecraft
+        mc = Minecraft.create()
+
+        # Post the message to chat
+        mc.postToChat(message)
+
+    # Probably didn't connect to Minecraft
+    except ConnectionRefusedError:
+
+        # Tell the user the connection failed
+        print("> Couldn't connect to Minecraft")
+
+    # The user wants to stop the program
+    except KeyboardInterrupt:
+
+        # Break out of the loop
+        break
